3109aee62fab98d1c1f06dd943eeec0751d00a54
Silgai <Silgai@yandex.ru>
2021-10-31 11:20:20 +0300

    Add project files.

---
+++ SkillFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SkillFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+
+            MyName = "Alexandr";
+
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
